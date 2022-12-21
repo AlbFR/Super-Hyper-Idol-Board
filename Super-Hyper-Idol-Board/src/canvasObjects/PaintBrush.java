d99b423d38704e5f84dc588fcde86d7b1d74f295
AlbFR <albertoferrada20@gmail.com>
2022-12-21 04:10:55 -0300

    LinkedList on PaintBrush.java(now serializable) replaced by ArrayList

--- Super-Hyper-Idol-Board/src/canvasObjects/PaintBrush.java
+++ Super-Hyper-Idol-Board/src/canvasObjects/PaintBrush.java
@@ -1,27 +1,28 @@
 package canvasObjects;
 
-import java.util.LinkedList;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class PaintBrush {
-    LinkedList <Integer> x = new LinkedList();
-    LinkedList <Integer> y = new LinkedList();
+public class PaintBrush implements Serializable {
+    ArrayList <Integer> x = new ArrayList();
+    ArrayList <Integer> y = new ArrayList();
     
     public void SavePoints(int x, int y){
         this.x.add(x);
         this.y.add(y);
     }
 
-    public LinkedList <Integer> XList(){
+    public ArrayList <Integer> XList(){
         return this.x;
     }
 
-    public LinkedList <Integer> YList(){
+    public ArrayList <Integer> YList(){
         return this.y;
     }
 
     public void eraseAll() {
-        x = new LinkedList();
-        y = new LinkedList();
+        x = new ArrayList();
+        y = new ArrayList();
     }
     
 }
